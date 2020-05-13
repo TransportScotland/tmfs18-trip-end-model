533ab7ea4b94410976759af9626af195b2dea168
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2020-05-13 18:16:02 +0100

    Add build script for executable

---
+++ build_exe.py
@@ -0,0 +1,9 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    '--name', 'TMfS18 Trip End Model',
+    '--onefile',
+    '--hidden-import', 'pkg_resources.py2_warn',
+    '--noconsole',      # comment out if errors occur, and run from command line
+    'gui.py'
+])
