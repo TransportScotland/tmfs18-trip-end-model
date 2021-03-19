690c88a1d734e2efc67272108bba53e488d9577f
jPeachPBA <jacob.peach@stantec.com>
2021-03-19 14:24:12 +0000

    Fixed exe build issue

--- build_exe.py
+++ build_exe.py
@@ -4,6 +4,6 @@ PyInstaller.__main__.run([
     '--name', 'TMfS18 Trip End Model',
     '--onefile',
     '--hidden-import', 'pkg_resources.py2_warn',
-    # '--noconsole',  # comment out if errors occur, and run from command line
+    '--noconsole',  # comment out if errors occur, and run from command line
     'gui.py'
 ])
